108115bbde6448949b6768c52923eb7c38f3c7c6
ncip48 <mbahcip00@gmail.com>
2022-03-23 22:00:10 +0700

    update seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,10 +19,14 @@ class ProductSeeder extends Seeder
         DB::table('products')->insert([
             [
                 'name' => 'Sintetis',
+                'image' => 'https://assets.skor.id/crop/0x0:0x0/x/photo/2020/08/15/2110142668.jpg',
+                'description' => 'asdf',
                 'price' => '15000'
             ],
             [
                 'name' => 'Non Sintetis',
+                'image' => 'https://www.lantaifutsal.com/wp-content/uploads/2019/10/Rumput-sintetis-outdoor-1.jpg',
+                'description' => 'gdherth',
                 'price' => '25000'
             ],
         ]);
